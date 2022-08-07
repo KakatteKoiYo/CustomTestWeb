1837e420525954c5863f46ab7b1ff41f345a1461
Daniel Romo <nintendofullpower@hotmail.com>
2022-08-06 20:58:33 -0500

    new

--- src/app/main-container/start-test/start-test.component.ts
+++ src/app/main-container/start-test/start-test.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, RouterLinkActive } from '@angular/router';
 import { DatosService, IListaPalabras } from 'src/app/datos.service';
 import {
